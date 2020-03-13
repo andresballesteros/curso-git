6c69f48b906033726e6a2847939fbea447f468b5
andres.ballesteros <ing.andres.ballesteros@hotmail.com>
2020-03-13 16:06:46 -0500

    prueba de git desde editor de codigo

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("Hello world");
+function hola(x, y) {
+  if (x - y < 0) {
+    console.log("Es menor que cero");
+  } else {
+    console.log("Es mayor que cero");
+  }
+}
+
+hola(1, 2);
